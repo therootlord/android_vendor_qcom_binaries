d898e9eff2054b7c18ec2c54b2e47a126ba962b0
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-02-19 05:40:40 -0500

    msm8916: Add proper license

--- msm8916/audio/libqct_resampler/qct_resampler.cpp
+++ msm8916/audio/libqct_resampler/qct_resampler.cpp
@@ -1,7 +1,22 @@
-/*============================================================================
-	QCT_Resampler Wrapper
-	Copyright (c) Vincent Zvikaramba
-============================================================================*/
+/**
+ * QCT Resampler Wrapper
+ *
+ * Copyright (C) 2017 Vincent Zvikaramba <vincent.zvikaramba@mail.utoronto.ca>
+ *
+ * liqct_resampler_wrapper is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This source is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the source code.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 #include <stdlib.h>
 #include <stdio.h>
